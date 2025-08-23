46028a6cc6e256c8dc7b07aca320f2383666d24b
Lucky44457 <maribelwate@gmail.com>
2025-08-23 15:16:44 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -1,28 +1,16 @@
----------------------------------------------------
-
-File Name: shrink.py
-
-Description: A Pyrogram bot for downloading files from Telegram channels or groups
-
-and uploading them back to Telegram.
-
-Author: Gagan
-
-GitHub: https://github.com/devgaganin/
-
-Telegram: https://t.me/PdfsHubbb
-
-YouTube: https://youtube.com/@dev_gagan
-
-Created: 2025-01-11
-
-Last Modified: 2025-01-11
-
-Version: 2.0.5
-
-License: MIT License
-
----------------------------------------------------
+# ---------------------------------------------------
+# File Name: shrink.py
+# Description: A Pyrogram bot for downloading files from Telegram channels or groups
+#              and uploading them back to Telegram.
+# Author: Gagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/PdfsHubbb
+# YouTube: https://youtube.com/@dev_gagan
+# Created: 2025-01-11
+# Last Modified: 2025-01-11
+# Version: 2.0.5
+# License: MIT License
+# ---------------------------------------------------
 
 from pyrogram import Client, filters from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup import random import requests import string import aiohttp from devgagan import app from devgagan.core.func import * from datetime import datetime, timedelta from motor.motor_asyncio import AsyncIOMotorClient from config import MONGO_DB, WEBSITE_URL, AD_API, LOG_GROUP
 
