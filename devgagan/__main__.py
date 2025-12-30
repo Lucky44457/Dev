f5476c24dc8a20010577ec4919faa092d2e5e10d
Lucky44457 <maribelwate@gmail.com>
2025-12-30 15:52:46 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -17,32 +17,22 @@ async def schedule_expiry_check():
         gc.collect()
 
 async def main():
-    # START BOT
     await app.start()
     print("Bot started")
 
-    # START USERBOT
     if userbot:
         await userbot.start()
         from devgagan.userbot_logger import attach_logger
         attach_logger(userbot)
-        print("Userbot started + logger attached")
+        print("Userbot started")
 
-    # START TELETHON
     await telethon_client.start(bot_token=app.bot_token)
 
-    # LOAD MODULES AFTER START
     for module in ALL_MODULES:
         importlib.import_module(f"devgagan.modules.{module}")
 
     asyncio.create_task(schedule_expiry_check())
-
     await idle()
-
-    # STOP
-    if userbot:
-        await userbot.stop()
-    await app.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
